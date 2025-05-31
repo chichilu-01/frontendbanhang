0a73507461ccb18f9819cfb87d1e4c47615b0fe6
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-05-31 06:57:49 +0000

    f

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
 import { ToastProvider } from "./context/ToastContext";
+import { MiniCartProvider } from "./context/MiniCartContext"; // ⬅️ Thêm dòng này
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <ToastProvider>
-        <App />
+        <MiniCartProvider>
+          {" "}
+          {/* ✅ Bọc App trong MiniCartProvider */}
+          <App />
+        </MiniCartProvider>
       </ToastProvider>
     </AuthProvider>
   </React.StrictMode>,

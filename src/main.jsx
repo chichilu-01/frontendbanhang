029913b49167aabc2cf0182d7709867d292cc817
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-11-03 05:24:11 +0000

    new4

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import App from "@/App.jsx";
+import "@/index.css";
 import { AuthProvider } from "@context/AuthContext";
 import { CartProvider } from "@context/CartContext";
 import { Toaster } from "react-hot-toast";
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       "@features": path.resolve(__dirname, "src/features"),
       "@assets": path.resolve(__dirname, "src/assets"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   server: {

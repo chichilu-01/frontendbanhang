f93bfa329432156596a7df8f9743cd37bdf3345b
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-11-05 07:41:26 +0000

    H

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+import { defineConfig } from "tailwindcss";
+
+export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});

cc2a2c67eccf941db57aca521c9cfbc5a4e5dcdb
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-12-01 05:51:01 +0000

    H

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,21 +4,20 @@ import LoginForm from "@components/LoginForm";
 export default function Login() {
   return (
     <div
-      className="min-h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat relative"
+      className="min-h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: "url('/bg-login.jpg')",
       }}
     >
-      {/* FORM LOGIN — glassmorphism */}
+      {/* FORM LOGIN */}
       <div
         className="
-          relative z-10 
-          w-full max-w-md
-          p-8
-          rounded-2xl
+          w-[90%] max-w-md 
+          p-8 
+          rounded-2xl 
           bg-white/20 
-          backdrop-blur-md 
-          shadow-2xl
+          backdrop-blur-xl 
+          shadow-2xl 
           border border-white/30
         "
       >

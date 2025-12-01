b10579c759fe80b492d9906c4562dad1b087a5ee
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-12-01 05:43:43 +0000

    H

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,5 +2,20 @@ import React from "react";
 import LoginForm from "@components/LoginForm";
 
 export default function Login() {
-  return <LoginForm />;
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `url("/bg-login.jpg")`,
+      }}
+    >
+      {/* lớp phủ mờ nền để form nổi bật */}
+      <div className="absolute inset-0 bg-white/60 backdrop-blur-sm"></div>
+
+      {/* form đăng nhập */}
+      <div className="relative z-10">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }

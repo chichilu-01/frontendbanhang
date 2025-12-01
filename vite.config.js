69902d58ba485b3d284124934c08eb51aa503683
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-12-01 02:40:08 +0000

    H

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@": path.resolve(__dirname, "./src"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
     },
   },
   server: {

93f439782997fc0099080e8964dcdf0a08b0c598
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-12-01 06:05:42 +0000

    H

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,23 +4,12 @@ import LoginForm from "@components/LoginForm";
 export default function Login() {
   return (
     <div
-      className="min-h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat"
+      className="min-h-screen w-full flex items-center justify-center bg-cover bg-center"
       style={{
         backgroundImage: "url('/bg-login.jpg')",
       }}
     >
-      {/* FORM LOGIN */}
-      <div
-        className="
-          w-[90%] max-w-md 
-          p-8 
-          rounded-2xl 
-          bg-white/20 
-          backdrop-blur-xl 
-          shadow-2xl 
-          border border-white/30
-        "
-      >
+      <div className="w-[90%] max-w-md p-8 bg-white/10 backdrop-blur-xl rounded-3xl shadow-2xl border border-white/20">
         <LoginForm />
       </div>
     </div>

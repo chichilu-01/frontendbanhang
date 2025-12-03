b4d37c108770097acf5df0eb48e2d82e36b88e95
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-12-03 07:33:50 +0000

    H

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ export default function Footer() {
           relative bg-white/40 backdrop-blur-2xl 
           border-t border-white/30 
           shadow-[0_8px_30px_rgba(0,0,0,0.15)]
-          bg-[url('public/footerimage.jpg')]
+          bg-[url('/footerimage.jpg')]
           bg-cover bg-center
           rounded-t-3xl
         "
